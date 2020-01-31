677202e05fd01db69d353e9df913c60c9ca8c620
Khoroshavin <khoroshavin.russia@gmail.com>
2020-01-31 12:12:55 +0700

    basic

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//BTN BURGER
+const menuToggle = document.querySelector('#menu-togle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active');
+}
+
+//SCROLL EBANY
+jQuery(function($){
+$('a[href*="#"]').on('click.smoothscroll', function( e ){
+var hash    = this.hash, _hash   = hash.replace(/#/,''), theHref = $(this).attr('href').replace(/#.*/, '');
+if( theHref && location.href.replace(/#.*/,'') != theHref ) return;
+var $target = _hash === '' ? $('body') : $( hash + ', a[name="'+ _hash +'"]').first();
+if( ! $target.length ) return;
+e.preventDefault();
+$('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 400, 'swing', function(){
+window.location.hash = hash;
+});
+});
+});
+
+$.ionTabs("#tabs_1"); // one tabs group
+
+// GALERY
+baguetteBox.run('.grid-gallery', { animation: 'slideIn'});
